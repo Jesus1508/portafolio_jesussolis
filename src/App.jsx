479e252fb6e8ea59783a6e81750a333eb0b05929
Jesus Solis <jesussolis.dev.23@gmail.com>
2025-04-19 12:16:49 -0600

    Ahora sí, Iniciando proyecto de portafolio personal jesus Solis

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-center text-3xl font-bold p-10">
+      Portafolio de Carmen de Jesús Solís Martínez 🚀
+    </div>
+  )
+}
+
+export default App;
